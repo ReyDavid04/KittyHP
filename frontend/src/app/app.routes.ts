4c9c324c21d3b1d1212f846c921025ffdd55b970
Rey David Ramos Murillo <davidramos342004@gmail.com>
2026-07-17 18:16:50 -0600

    Agregar ruta para visualizar reportes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { CatalogManagementPageComponent } from './features/repairs/pages/catalog-management-page.component';
 import { RepairEditorPageComponent } from './features/repairs/pages/repair-editor-page.component';
+import { RepairViewPageComponent } from './features/repairs/pages/repair-view-page.component';
 import { RepairsPageComponent } from './features/repairs/pages/repairs-page.component';
 
 export const appRoutes: Routes = [
   { path: '', component: RepairsPageComponent },
   { path: 'repairs/new', component: RepairEditorPageComponent },
+  { path: 'repairs/:id/view', component: RepairViewPageComponent },
   { path: 'repairs/:id/edit', component: RepairEditorPageComponent },
   { path: 'settings/catalogs/:type', component: CatalogManagementPageComponent },
   { path: 'settings', redirectTo: 'settings/catalogs/top_issue', pathMatch: 'full' },
