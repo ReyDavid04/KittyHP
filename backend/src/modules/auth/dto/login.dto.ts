6d204e999d97db0351263ad75248d1de9fbc51e9
Rey David Ramos Murillo <davidramos342004@gmail.com>
2026-07-20 15:25:32 -0600

    Usar correo en inicio de sesión

--- backend/src/modules/auth/dto/login.dto.ts
+++ backend/src/modules/auth/dto/login.dto.ts
@@ -3,7 +3,7 @@ import { IsString, MinLength } from 'class-validator';
 export class LoginDto {
   @IsString()
   @MinLength(1)
-  username!: string;
+  email!: string;
 
   @IsString()
   @MinLength(1)
