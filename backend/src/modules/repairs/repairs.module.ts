0786a22fc05797b044bf9945fb234151699d8d18
Rey David Ramos Murillo <davidramos342004@gmail.com>
2026-07-20 14:33:18 -0600

    Conectar autenticación al módulo de reparaciones

--- backend/src/modules/repairs/repairs.module.ts
+++ backend/src/modules/repairs/repairs.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
 import { RepairsController } from './controllers/repairs.controller';
 import { RepairEntity } from './entities/repair.entity';
 import { RepairRepository } from './repositories/repair.repository';
@@ -7,7 +8,7 @@ import { RepairsService } from './services/repairs.service';
 import { CreateRepairUseCase } from './use-cases/create-repair.use-case';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RepairEntity])],
+  imports: [TypeOrmModule.forFeature([RepairEntity]), AuthModule],
   controllers: [RepairsController],
   providers: [RepairRepository, RepairsService, CreateRepairUseCase],
 })
