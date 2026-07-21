a8fe753c3bc0fb2a806015e10abcd8b65a7bca51
Rey David Ramos Murillo <davidramos342004@gmail.com>
2026-07-21 11:15:40 -0600

    Usar firma explícita para usuario creador

--- backend/src/modules/repairs/use-cases/create-repair.use-case.ts
+++ backend/src/modules/repairs/use-cases/create-repair.use-case.ts
@@ -2,19 +2,17 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateRepairDto } from '../dto/create-repair.dto';
 import { RepairRepository } from '../repositories/repair.repository';
 
-type CreateRepairWithCreator = CreateRepairDto & {
-  createdByUserId?: number;
-};
-
 @Injectable()
 export class CreateRepairUseCase {
-  execute(createRepairDto: CreateRepairDto, repairRepository: RepairRepository) {
-    const createdByUserId = (createRepairDto as CreateRepairWithCreator).createdByUserId;
-
-    if (!Number.isInteger(createdByUserId) || Number(createdByUserId) <= 0) {
+  execute(
+    createRepairDto: CreateRepairDto,
+    createdByUserId: number,
+    repairRepository: RepairRepository,
+  ) {
+    if (!Number.isInteger(createdByUserId) || createdByUserId <= 0) {
       throw new UnauthorizedException('No fue posible identificar al usuario que crea el reporte.');
     }
 
-    return repairRepository.create(createRepairDto, Number(createdByUserId));
+    return repairRepository.create(createRepairDto, createdByUserId);
   }
 }
