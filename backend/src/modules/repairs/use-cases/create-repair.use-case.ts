0f15435b5efbafcdb84cfc14f3dc9c78d88e06a1
Rey David Ramos Murillo <davidramos342004@gmail.com>
2026-07-21 11:02:02 -0600

    Validar usuario autenticado al crear reparaciones

--- backend/src/modules/repairs/use-cases/create-repair.use-case.ts
+++ backend/src/modules/repairs/use-cases/create-repair.use-case.ts
@@ -1,14 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateRepairDto } from '../dto/create-repair.dto';
 import { RepairRepository } from '../repositories/repair.repository';
 
+type CreateRepairWithCreator = CreateRepairDto & {
+  createdByUserId?: number;
+};
+
 @Injectable()
 export class CreateRepairUseCase {
-  execute(
-    createRepairDto: CreateRepairDto,
-    createdByUserId: number,
-    repairRepository: RepairRepository,
-  ) {
-    return repairRepository.create(createRepairDto, createdByUserId);
+  execute(createRepairDto: CreateRepairDto, repairRepository: RepairRepository) {
+    const createdByUserId = (createRepairDto as CreateRepairWithCreator).createdByUserId;
+
+    if (!Number.isInteger(createdByUserId) || Number(createdByUserId) <= 0) {
+      throw new UnauthorizedException('No fue posible identificar al usuario que crea el reporte.');
+    }
+
+    return repairRepository.create(createRepairDto, Number(createdByUserId));
   }
 }
